e0735571b21a5e142db2d71f3fae1abe0137ee13
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-07-21 13:53:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# React boilerplate
+
+Initial setup for React with Typescript projects.
+
+## Installation
+
+Clone the repository, then run:
+
+```bash
+npm install
+```
+
+## Usage
+
+Development mode
+
+```bash
+npm start
+```
+
+Generate build
+
+```bash
+npm run build
+```
+
+Serve build
+
+```bash
+npx webpack serve
+```
+
+## Dependencies
+
+### Development
+
+<ul> 
+  <li> @babel/core </li>
+  <li> @babel/preset-env </li>
+  <li> @babel/preset-react </li>
+  <li> @babel/preset-typescript </li>
+  <li> @types/react </li>
+  <li> @types/react-dom </li>
+  <li> @types/styled-components </li>
+  <li> babel-loader </li>
+  <li> clean-webpack-plugin </li>
+  <li> html-loader </li>
+  <li> html-webpack-plugin </li>
+  <li> source-map-loader </li>
+  <li> styled-components </li>
+  <li> terser-webpack-plugin </li>
+  <li> ts-loader </li>
+  <li> typescript </li>
+  <li> webpack </li>
+  <li> webpack-cli </li>
+  <li> webpack-dev-server </li>
+</ul>
+
+### Production
+
+<ul>
+  <li> react </li>
+  <li> react-dom </li>
+</ul>
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+    },
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [{ loader: "babel-loader" }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: [{ loader: "ts-loader" }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
